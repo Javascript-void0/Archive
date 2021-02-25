21e0a3b8bd6e8a5957de224b4406e68b4360a044
Java <indead1124@gmail.com>
2021-02-25 15:30:13 -0500

    Lines Embed

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Around the Clock Archives"))
 
 @client.command(aliases=['s'], help='Search Indexes')
-async def search(ctx, index=None):
-    #text = []
+async def search(ctx, index=None, p=None):
     if not index:
         await ctx.send('.search <index1 | index2 | index3>')
     else:
-#        with open(f'index/{index}/test.txt', "rb") as file:
-#            await ctx.send("Your file is:", file=discord.File(file, "test.txt"))
-
+        if not p:
+            p = 1
+        if p <= 0:
+            p = 1
         with open(f'index/{index}/test.txt', 'r') as file:
-            title, body = file.read().split(" SPLIT ")
-            embed = discord.Embed(title=f'{index}. Document', description='Page NUM')
-            embed.add_field(name=title, value=body)
-            await ctx.send(embed=embed)
-    
+            lines = file.read().splitlines()
+            for s in lines:
+                title, body = s.split(" SPLIT ")
+                embed = discord.Embed(title=f'{index}. Document', description=f'Page {p}')
+                embed.add_field(name=title, value=body)
+                await ctx.send(embed=embed)
+
 if __name__ == '__main__':
     client.run(TOKEN)
