53b9713b6aa1d5deb28a8c5d50f6df09b2d5b85f
Java <indead1124@gmail.com>
2021-02-25 13:08:56 -0500

    Figuring Stuff, example docs :|

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ async def on_ready():
 
 @client.command(aliases=['s'], help='Search Indexes')
 async def search(ctx, index=None):
+    #text = []
     if not index:
         await ctx.send('.search <index1 | index2 | index3>')
     else:
-        with open(f'index/{index}/test.txt', 'r') as file:
-            await ctx.send(file.read().strip())
+#        with open(f'index/{index}/test.txt', "rb") as file:
+#            await ctx.send("Your file is:", file=discord.File(file, "test.txt"))
 
+        with open(f'index/{index}/test.txt', 'r') as file:
+            title, body = file.read().split(" SPLIT ")
+            embed = discord.Embed(title=f'{index}. Document', description='Page NUM')
+            embed.add_field(name=title, value=body)
+            await ctx.send(embed=embed)
+    
 if __name__ == '__main__':
-    client.run(TOKEN)
+    client.run('ODE0Mjg1NjUyMTE1OTgwMzcw.YDbonA.kSPMPXSj2G2WYh5w3rtb62vnVz0')
