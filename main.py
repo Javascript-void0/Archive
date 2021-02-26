603287d82b142ab31b34bd35469d11f474e5e179
Java <indead1124@gmail.com>
2021-02-26 13:06:33 -0500

    Images :D

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-async def update_embed(page, edir, message, lines):
-    title, body = lines[page].split(" SPLIT ")
-    embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
+async def update_embed(page, edir, message, lines, image=None):
+    try:
+        title, body = lines[page].split(" SPLIT ")
+    except ValueError:
+        title, body, image = lines[page].split(" SPLIT ")
+        embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1} - [Image]({image})')
+    else:
+        embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
     embed.add_field(name=title, value=body)
     await message.edit(embed=embed)
 
@@ -26,8 +31,13 @@ async def search(ctx, dir=None):
         lines = file.read().splitlines()
         pages = len(lines)
         page = 0
-        title, body = lines[page].split(" SPLIT ")
-        embed = discord.Embed(title=f'{edir} Document', description=f'Page {page+1}')
+        try:
+            title, body = lines[page].split(" SPLIT ")
+        except ValueError:
+            title, body, image = lines[page].split(" SPLIT ")
+            embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1} - [Image]({image})')
+        else:
+            embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
         embed.add_field(name=title, value=body)
         message = await ctx.send(embed=embed)
         await message.add_reaction("⏮")
@@ -42,21 +52,21 @@ async def search(ctx, dir=None):
         while True:
             try:
                 reaction, user = await client.wait_for('reaction_add', timeout= 60.0, check=check)
-                if reaction.emoji == '⏮':
+                if reaction.emoji == '⏮' and page != 0:
                     page = 0
-                    await update_embed(page, edir, message, lines)
+                    await update_embed(page, edir, message, lines, image)
                     await message.remove_reaction(reaction, user)
                 elif reaction.emoji == '◀' and page > 0:
                     page -= 1
-                    await update_embed(page, edir, message, lines)
+                    await update_embed(page, edir, message, lines, image)
                     await message.remove_reaction(reaction, user)
                 elif reaction.emoji == '▶' and page < pages -1:
                     page += 1
-                    await update_embed(page, edir, message, lines)
+                    await update_embed(page, edir, message, lines, image)
                     await message.remove_reaction(reaction, user)
-                elif reaction.emoji == '⏭':
+                elif reaction.emoji == '⏭' and page != len(lines)-1:
                     page = len(lines)-1
-                    await update_embed(page, edir, message, lines)
+                    await update_embed(page, edir, message, lines, image)
                     await message.remove_reaction(reaction, user)
                 elif reaction.emoji == '❌':
                     await message.edit(content='`Timeout`', embed=embed)
@@ -67,14 +77,12 @@ async def search(ctx, dir=None):
                 await message.edit(content='`Timeout`', embed=embed)
                 break
 
-@client.command(aliases=['files'], help='List Files in Directory')
+@client.command(aliases=['files, index'], help='List Files in Directory')
 async def list(ctx):
     for file in os.listdir('index'):
         if file.startswith("1_"):
-            file = file[2:]
             await ctx.send(f"**US_History**: {file}")
         elif file.startswith("2_"):
-            file = file[2:]
             await ctx.send(f"**Testing**: {file}")
 
 if __name__ == '__main__':
