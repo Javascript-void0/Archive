9657531e8039dc57c05f3cfd6463260d62655523
Java <indead1124@gmail.com>
2021-02-25 19:36:48 -0500

    Move out index

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-async def update_embed(page, index, message, lines):
+async def update_embed(page, dir, message, lines):
     title, body = lines[page].split(" SPLIT ")
-    embed = discord.Embed(title=f'{index}. Document', description=f'Page {page+1}')
+    embed = discord.Embed(title=f'{dir}. Document', description=f'Page {page+1}')
     embed.add_field(name=title, value=body)
     await message.edit(embed=embed)
 
@@ -19,55 +19,52 @@ async def on_ready():
     print('Started {0.user}'.format(client))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Around the Clock Archives"))
 
-@client.command(aliases=['s'], help='Search Indexes')
-async def search(ctx, index=None):
-    if not index:
-        await ctx.send('.search <i | ii | iii>')
-    else:
-        with open(f'index/{index}/test.txt', 'r') as file:
-            lines = file.read().splitlines()
-            pages = len(lines)
-            page = 0
-            title, body = lines[page].split(" SPLIT ")
-            embed = discord.Embed(title=f'{index}. Document', description=f'Page {page+1}')
-            embed.add_field(name=title, value=body)
-            message = await ctx.send(embed=embed)
-            await message.add_reaction("⏮")
-            await message.add_reaction("◀")
-            await message.add_reaction("▶")
-            await message.add_reaction("⏭")
-            await message.add_reaction("❌")
+@client.command(aliases=['s'], help='Search Directory')
+async def search(ctx, dir=None):
+    with open(f'index/{dir}.txt', 'r') as file:
+        lines = file.read().splitlines()
+        pages = len(lines)
+        page = 0
+        title, body = lines[page].split(" SPLIT ")
+        embed = discord.Embed(title=f'{dir}. Document', description=f'Page {page+1}')
+        embed.add_field(name=title, value=body)
+        message = await ctx.send(embed=embed)
+        await message.add_reaction("⏮")
+        await message.add_reaction("◀")
+        await message.add_reaction("▶")
+        await message.add_reaction("⏭")
+        await message.add_reaction("❌")
 
-            def check(reaction, user):
-                return reaction.message.id == message.id and user == ctx.author
+        def check(reaction, user):
+            return reaction.message.id == message.id and user == ctx.author
 
-            while True:
-                try:
-                    reaction, user = await client.wait_for('reaction_add', timeout= 60.0, check=check)
-                    if reaction.emoji == '⏮':
-                        page = 0
-                        await update_embed(page, index, message, lines)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '◀' and page > 0:
-                        page -= 1
-                        await update_embed(page, index, message, lines)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '▶' and page < pages -1:
-                        page += 1
-                        await update_embed(page, index, message, lines)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '⏭':
-                        page = len(lines)-1
-                        await update_embed(page, index, message, lines)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '❌':
-                        await message.edit(content='`Timeout`', embed=embed)
-                        break
-                    else:
-                        await message.remove_reaction(reaction, user)
-                except asyncio.TimeoutError:
+        while True:
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout= 60.0, check=check)
+                if reaction.emoji == '⏮':
+                    page = 0
+                    await update_embed(page, dir, message, lines)
+                    await message.remove_reaction(reaction, user)
+                elif reaction.emoji == '◀' and page > 0:
+                    page -= 1
+                    await update_embed(page, dir, message, lines)
+                    await message.remove_reaction(reaction, user)
+                elif reaction.emoji == '▶' and page < pages -1:
+                    page += 1
+                    await update_embed(page, dir, message, lines)
+                    await message.remove_reaction(reaction, user)
+                elif reaction.emoji == '⏭':
+                    page = len(lines)-1
+                    await update_embed(page, dir, message, lines)
+                    await message.remove_reaction(reaction, user)
+                elif reaction.emoji == '❌':
                     await message.edit(content='`Timeout`', embed=embed)
                     break
+                else:
+                    await message.remove_reaction(reaction, user)
+            except asyncio.TimeoutError:
+                await message.edit(content='`Timeout`', embed=embed)
+                break
 
 if __name__ == '__main__':
     client.run(TOKEN)
