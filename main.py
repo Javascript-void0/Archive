4ff671634aea79418ffaccd218c211d4c4f9404d
Java <indead1124@gmail.com>
2021-02-27 01:20:07 -0500

    .list fix?

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
+doc_count = 0
+cat_count = 0
 
-for file in os.listdir('index'):
-    ct = []
-    ct.append(file[0])
-    ct.sort()
-    doc_count = len(ct)
-    cat_count = int(ct[-1])
+async def file_count():
+    for file in os.listdir('index'):
+        global doc_count, cat_count
+        ct = []
+        ct.append(file[0])
+        ct.sort()
+        doc_count = len(ct)
+        cat_count = int(ct[-1])
 
 async def update_embed(page, f, message, lines, image=None):
     try:
@@ -37,6 +41,7 @@ async def search(ctx, dir=None, option=None):
         await ctx.send('Usage: `.search <file> <all>`')
 
     global doc_count
+    await file_count()
     for file in os.listdir('index'):
         f = file[2:]
         if f == dir + ".txt":
@@ -109,6 +114,7 @@ async def search(ctx, dir=None, option=None):
 @client.command(aliases=['files, index'], help='List Files in Directory')
 async def list(ctx):
     global cat_count
+    await file_count()
     l = {}
     em = discord.Embed(title="List of Files in Directory")
     for i in range(cat_count):
