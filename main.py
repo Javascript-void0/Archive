287491485e54d3f3a5abce6b55cace7aa9d2e5da
Java <indead1124@gmail.com>
2021-02-27 01:13:05 -0500

    Search without file beginning

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-async def update_embed(page, edir, message, lines, image=None):
+for file in os.listdir('index'):
+    ct = []
+    ct.append(file[0])
+    ct.sort()
+    doc_count = len(ct)
+    cat_count = int(ct[-1])
+
+async def update_embed(page, f, message, lines, image=None):
     try:
         title, body = lines[page].split(" SPLIT ")
     except ValueError:
         title, body, image = lines[page].split(" SPLIT ")
-        embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1} - [Image]({image})')
+        embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1} - [Image]({image})')
     else:
-        embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
+        embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1}')
     embed.add_field(name=title, value=body)
     await message.edit(embed=embed)
 
@@ -29,82 +36,82 @@ async def search(ctx, dir=None, option=None):
     if dir == None:
         await ctx.send('Usage: `.search <file> <all>`')
 
-    edir = dir[2:]
-    with open(f'index/{dir}.txt', 'r') as file:
-        lines = file.read().splitlines()
-        pages = len(lines)
-        page = 0
-        image = None
+    global doc_count
+    for file in os.listdir('index'):
+        f = file[2:]
+        if f == dir + ".txt":
+            with open(f'index/{file}', 'r') as file:
+                lines = file.read().splitlines()
+                pages = len(lines)
+                page = 0
+                image = None
 
-        if option == "all":
-            for s in lines:
-                try:
-                    title, body = s.split(" SPLIT ")
-                except ValueError:
-                    title, body, image = s.split(" SPLIT ")
-                    embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1} - [Image]({image})')
-                else:
-                    embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
-                embed.add_field(name=title, value=body)
-                page += 1
-                message = await ctx.send(embed=embed)
-
-        else:
-            try:
-                title, body = lines[page].split(" SPLIT ")
-            except ValueError:
-                title, body, image = lines[page].split(" SPLIT ")
-                embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1} - [Image]({image})')
-            else:
-                embed = discord.Embed(title=f'Document {edir}', description=f'Page {page+1}')
-            embed.add_field(name=title, value=body)
-            message = await ctx.send(embed=embed)
-            await message.add_reaction("⏮")
-            await message.add_reaction("◀")
-            await message.add_reaction("▶")
-            await message.add_reaction("⏭")
-            await message.add_reaction("❌")
-
-            def check(reaction, user):
-                return reaction.message.id == message.id and user == ctx.author
-
-            while True:
-                try:
-                    reaction, user = await client.wait_for('reaction_add', timeout= 60.0, check=check)
-                    if reaction.emoji == '⏮' and page != 0:
-                        page = 0
-                        await update_embed(page, edir, message, lines, image)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '◀' and page > 0:
-                        page -= 1
-                        await update_embed(page, edir, message, lines, image)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '▶' and page < pages -1:
+                if option == "all":
+                    for s in lines:
+                        try:
+                            title, body = s.split(" SPLIT ")
+                        except ValueError:
+                            title, body, image = s.split(" SPLIT ")
+                            embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1} - [Image]({image})')
+                        else:
+                            embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1}')
+                        embed.add_field(name=title, value=body)
                         page += 1
-                        await update_embed(page, edir, message, lines, image)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '⏭' and page != len(lines)-1:
-                        page = len(lines)-1
-                        await update_embed(page, edir, message, lines, image)
-                        await message.remove_reaction(reaction, user)
-                    elif reaction.emoji == '❌':
-                        await message.edit(content='`Timeout`', embed=embed)
-                        break
+                        message = await ctx.send(embed=embed)
+
+                else:
+                    try:
+                        title, body = lines[page].split(" SPLIT ")
+                    except ValueError:
+                        title, body, image = lines[page].split(" SPLIT ")
+                        embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1} - [Image]({image})')
                     else:
-                        await message.remove_reaction(reaction, user)
-                except asyncio.TimeoutError:
-                    await message.edit(content='`Timeout`', embed=embed)
-                    break
+                        embed = discord.Embed(title=f'Document {f}', description=f'Page {page+1}')
+                    embed.add_field(name=title, value=body)
+                    message = await ctx.send(embed=embed)
+                    await message.add_reaction("⏮")
+                    await message.add_reaction("◀")
+                    await message.add_reaction("▶")
+                    await message.add_reaction("⏭")
+                    await message.add_reaction("❌")
+
+                    def check(reaction, user):
+                        return reaction.message.id == message.id and user == ctx.author
+
+                    while True:
+                        try:
+                            reaction, user = await client.wait_for('reaction_add', timeout= 60.0, check=check)
+                            if reaction.emoji == '⏮' and page != 0:
+                                page = 0
+                                await update_embed(page, f, message, lines, image)
+                                await message.remove_reaction(reaction, user)
+                            elif reaction.emoji == '◀' and page > 0:
+                                page -= 1
+                                await update_embed(page, f, message, lines, image)
+                                await message.remove_reaction(reaction, user)
+                            elif reaction.emoji == '▶' and page < pages -1:
+                                page += 1
+                                await update_embed(page, f, message, lines, image)
+                                await message.remove_reaction(reaction, user)
+                            elif reaction.emoji == '⏭' and page != len(lines)-1:
+                                page = len(lines)-1
+                                await update_embed(page, f, message, lines, image)
+                                await message.remove_reaction(reaction, user)
+                            elif reaction.emoji == '❌':
+                                await message.edit(content='`Timeout`', embed=embed)
+                                break
+                            else:
+                                await message.remove_reaction(reaction, user)
+                        except asyncio.TimeoutError:
+                            await message.edit(content='`Timeout`', embed=embed)
+                            break
 
 @client.command(aliases=['files, index'], help='List Files in Directory')
 async def list(ctx):
+    global cat_count
     l = {}
-    ct = []
     em = discord.Embed(title="List of Files in Directory")
-    for file in os.listdir('index'):
-        ct.append(file[0])
-        ct.sort()
-    for i in range(int(ct[-1])):
+    for i in range(cat_count):
         i += 1
         l[i] = []
         for file in os.listdir('index'):
