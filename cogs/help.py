c8a490a47c9c2ad27f1674656e2ff6d6f16cad88
Java <indead1124@gmail.com>
2021-03-02 12:36:39 -0500

    whatever

--- cogs/help.py
+++ cogs/help.py
@@ -11,7 +11,7 @@ class Help(commands.Cog):
     @commands.command(aliases=['i','information','info'], help='Info about the bot')
     async def help(self, ctx):
         embed = discord.Embed(title='🗑 Archive Bot', description='Random crap coming from a [Github Repository](https://github.com/Javascript-void0/Archive).')
-        embed.add_field(name=f'Documents: {main.doc_count}', value='Made by Java  🍌', inline=True)
+        embed.add_field(name=f'Documents: {doc_count}', value='Made by Java  🍌', inline=True)
 #        embed.add_field(name='Commands: 3', value='help, search, list', inline=True)
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/814293652234043392/815606602316382208/totoro-560x301_3.jpg')
         await ctx.send(embed=embed)
--- config.py
+++ config.py
@@ -1,11 +1,16 @@
-import os
+# import os
 
-cat_count = 0
-doc_count = 0
-
+''' Inconsistent on Heroku >:|
 for file in os.listdir('index'):
     gr = file[0]
     doc_count += 1
     if gr < file[0]:
         gr = file[0]
 cat_count = int(gr)
+'''
+
+cat_count = 3
+doc_count = 0
+
+for file in os.listdir('index'):
+    doc_count += 1
